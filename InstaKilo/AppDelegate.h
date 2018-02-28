abcee49be20cac770ce6368d6d2ed4e7d9239baf
Christoper <celoranta@gmail.com>
2018-02-28 08:30:45 -0800

    Initial Commit

---
+++ InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InstaKilo
+//
+//  Created by Chris Eloranta on 2018-02-28.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
